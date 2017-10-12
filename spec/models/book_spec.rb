ceb0ea214f50f495e5c2d1ad7b6592473672221e
Guillermo Salazar <guillermos_2007@yahoo.com>
2017-10-12 13:58:17 -0400

    added book parser test file

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -8,13 +8,13 @@ describe Book, '#new' do
   end
   context 'with missing required values' do
     it 'should fail if title is missing' do
-      expect {Book.new({author: 'Russ Olsen', isbn: '978-0321584106'})}.to raise_error('No Title')
+      expect { Book.new({author: 'Russ Olsen', isbn: '978-0321584106'}) }.to raise_error('No Title')
     end
     it 'should fail if isbn is missing' do
-      expect {Book.new({title: 'Eloquent Ruby', author: 'Russ Olsen'})}.to raise_error('No ISBN')
+      expect { Book.new({title: 'Eloquent Ruby', author: 'Russ Olsen'}) }.to raise_error('No ISBN')
     end
     it 'should fail if author is missing' do
-      expect {Book.new({title: 'Eloquen Ruby', isbn: '978-0321584106'})}.to raise_error('No Author')
+      expect { Book.new({title: 'Eloquen Ruby', isbn: '978-0321584106'}) }.to raise_error('No Author')
     end
   end
 end
